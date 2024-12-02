7e9452bc5eda83681c6dc868cfe256ad93004c27
Farrukh Zaman <you@example.com>
2024-12-02 12:52:43 +0500

    basic operation

---
+++ main.py
@@ -0,0 +1,82 @@
+#   Questions
+
+# Basic Operations
+
+# 1.	Create a dictionary student with keys: name, age, and grade. Assign them appropriate values.
+
+
+# student = {
+#     "name": "Ali Khan",
+#     "age": 16,
+#     "grade": "A"
+# }
+
+# print(student)
+   
+
+# 2.	Access the value of the key grade in the student dictionary.
+
+
+# student = {
+#     "name": "Ali Khan",
+#     "age": 16,
+#     "grade": "A"
+# }
+
+
+# print(student["grade"])
+    
+
+
+# 3.	Add a new key city to the student dictionary and set its value to "New York".
+
+# student = {
+#     "name": "Ali Khan",
+#     "age": 16,
+#     "grade": "A"
+# }
+
+# student["city"] = "New York"
+# print(student)
+
+# {
+#     "name": "Ali Khan",
+#     "age": 16,
+#     "grade": "A",
+#     "city": "New York"
+# }
+
+
+
+# 4.	Update the value of the age key in the student dictionary to 20.
+
+
+# student = {
+#     "name": "Ali Khan",
+#     "age": 16,
+#     "grade": "A",
+#     "city": "New York"
+# }
+
+
+# student["age"] = 20
+
+
+# print(student)
+
+
+# 5.	Remove the key city from the student dictionary.
+
+
+# student = {
+#     "name": "Ali Khan",
+#     "age": 16,
+#     "grade": "A",
+#     "city": "New York"
+# }
+
+
+# student["age"] = 20
+
+
+# print(student)
